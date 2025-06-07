a2ae320d91233c79648cbbb7e39611d8b2b4f701
Jason Michael <jasonmic2000@gmail.com>
2025-06-07 15:58:04 +0530

    replace divs on homepage

--- components/Work.tsx
+++ components/Work.tsx
@@ -18,14 +18,14 @@ const Work = ({
       {workEntries.map((entry: WorkEntryMeta) => (
         <ul className="flex flex-col py-4" key={entry.startDate}>
           <li className="animate">
-            <div className="font-semibold">{entry.company}</div>
-            <div className="text-sm opacity-75">{entry.role}</div>
-            <div className="text-sm opacity-75">
+            <p className="font-semibold">{entry.company}</p>
+            <p className="text-sm opacity-75">{entry.role}</p>
+            <span className="text-sm opacity-75">
               {`${entry.startDate} - ${entry.endDate}`}
-            </div>
+            </span>
             <article>
               {entry.initialDetails && (
-                <div className="py-4 font-serif">{entry.initialDetails}</div>
+                <p className="py-4 font-serif">{entry.initialDetails}</p>
               )}
               {showDetails && (
                 <div
