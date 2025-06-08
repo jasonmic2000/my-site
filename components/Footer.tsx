617dd0c35998bdf0c536490dd7216b52970ec0ff
Jason Michael <jasonmic2000@gmail.com>
2025-06-08 22:20:22 +0530

    add hover and underline to footer, also link with github

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export const Footer = () => {
   return (
     <footer className="w-full py-5 pb-10 text-sm">
       <div className="mx-auto max-w-screen-sm px-4">
-        © 2025 | Jason Michael{" "}
+        © 2025 | <Link href="https://github.com/jasonmic2000" className="hover:text-black dark:hover:text-white transition duration-300 ease-in-out hover:underline">Jason Michael </Link>
       </div>
     </footer>
   );
