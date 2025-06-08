096b225e2897882cdbcfd31e4d339df2de809612
Jason Michael <jasonmic2000@gmail.com>
2025-06-08 20:05:23 +0530

    add metadata to works page

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,5 +1,11 @@
 import Work from "@/components/Work";
 import { getAllWorkEntries } from "@/lib/utils";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Work",
+  description: "My professional experience and roles.",
+};
 
 const WorkPage = async () => {
   const workEntries = await getAllWorkEntries();
