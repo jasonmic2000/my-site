8d2006165e30da8f0995cb1a9aedc2d706a08f32
Jason Michael <jasonmic2000@gmail.com>
2026-08-13 20:22:56 +0530

    chore: fix navbar return statement

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,16 +11,20 @@ const NavbarItems = [
   {
     name: "work",
     slug: "/work",
-  }
+  },
 ];
 
 const Navbar = () => {
   const { setTheme, resolvedTheme } = useTheme();
 
+  return (
     <header className="w-full lg:mb-16 mb-12 py-5">
       <div className="flex px-4 md:px-0 flex-row items-center justify-between">
         <div className="flex items-center">
-          <Link href="/" className="text-md font-semibold hover:text-black dark:hover:text-white transition duration-300 ease-in-out">
+          <Link
+            href="/"
+            className="text-md font-semibold hover:text-black dark:hover:text-white transition duration-300 ease-in-out"
+          >
             ¯\_(ツ)_/¯
           </Link>
         </div>
@@ -42,11 +46,12 @@ const Navbar = () => {
               setTheme(resolvedTheme === "dark" ? "light" : "dark")
             }
           >
-             <FiMoon />
+            <FiMoon />
           </button>
         </div>
       </div>
     </header>
+  );
 };
 
 export default Navbar;
